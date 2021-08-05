5f8dac93a0d2a40c75ae38f8bf6890ceef0ede1d
mvgreen <mvgreen342@gmail.com>
2021-08-05 17:13:17 +0400

    Instantiate an empty copy without fields

--- src/main/java/com/mvgreen/deepcopy/DeepCopyUtil.java
+++ src/main/java/com/mvgreen/deepcopy/DeepCopyUtil.java
@@ -1,6 +1,7 @@
 package com.mvgreen.deepcopy;
 
 import com.mvgreen.deepcopy.annotations.DeepCopyable;
+import com.mvgreen.deepcopy.exceptions.CloneException;
 import com.mvgreen.deepcopy.factories.CloneFactory;
 
 import java.lang.reflect.Constructor;
@@ -11,8 +12,47 @@ public class DeepCopyUtil {
 
     private Map<Class<?>, CloneFactory<?>> cloneFactories = new HashMap<>();
 
-    public <T> T deepCopy(T src) {
+    public void addCloneFactory(CloneFactory<?> factory) {
+        cloneFactories.put(factory.getCloneClass(), factory);
+    }
+
+    public void removeCloneFactory(Class<?> type) {
+        cloneFactories.remove(type);
+    }
+
+    public <T> T deepCopy(T src) throws CloneException {
         validateArgument(src);
+        Map<Object, Object> cloneReferences = new HashMap<>();
+        return deepCopy(src, cloneReferences);
+    }
+
+    private <T> T deepCopy(T src, Map<Object, Object> cloneReferences) {
+        if (src.getClass().isAnnotationPresent(DeepCopyable.class)) {
+            return copyDeepCopyableObject(src, cloneReferences);
+        } else {
+            return copyObjectWithFactory(src, cloneReferences);
+        }
+    }
+
+
+    private <T> T copyDeepCopyableObject(T src, Map<Object, Object> cloneReferences) {
+        Class<?> klass = src.getClass();
+        T clone;
+        try {
+            Constructor<?> constructor = klass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            //noinspection unchecked
+            clone = (T) constructor.newInstance();
+        } catch (Exception e) {
+            throw new CloneException(e);
+        }
+
+        cloneReferences.put(src, clone);
+        return clone;
+    }
+
+    // TODO
+    private <T> T copyObjectWithFactory(T src, Map<Object, Object> cloneReferences) {
         return null;
     }
 
@@ -37,14 +77,6 @@ public class DeepCopyUtil {
         if (!found) {
             throw new IllegalArgumentException("class " + klass + " does not have a constructor with no parameters");
         }
-    }
-
-    public void addCloneFactory(CloneFactory<?> factory) {
-        cloneFactories.put(factory.getCloneClass(), factory);
-    }
-
-    public void removeCloneFactory(Class<?> type) {
-        cloneFactories.remove(type);
     }
 
 }
---
+++ src/main/java/com/mvgreen/deepcopy/exceptions/CloneException.java
@@ -0,0 +1,9 @@
+package com.mvgreen.deepcopy.exceptions;
+
+public class CloneException extends RuntimeException {
+
+    public CloneException(Exception cause) {
+        super(cause);
+    }
+
+}
